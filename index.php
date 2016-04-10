f9fb34f84d85841edeb2506632903934688d85c1
clarissa_vazquez <cvasquez-ramo@csumb.edu>
2016-04-10 22:59:24 +0000

    Added a dropdown menu for user to sort results by

--- index.php
+++ index.php
@@ -9,7 +9,16 @@
     </head>
     <body>
         <h1>Online Catalog</h1>
-        
+        <form>
+            <select name="orderBy">
+                <option value="">Select One</option>
+                <option value="highToLow">High to Low</option>
+                <option value="lowToHigh">Low to High</option>
+                <option value="vehicleSize">Size</option>
+                <option value="vehicleType">Type</option>
+                <option value="vehicleYear">Year</option>
+            </select>
+        </form>
 
     </body>
 </html>
