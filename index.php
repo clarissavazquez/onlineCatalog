a7caf4a363be9d7e71f5cac2523f116743e3ddf8
clarissa_vazquez <cvasquez-ramo@csumb.edu>
2016-04-10 22:38:47 +0000

    Added a file

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Online Catalog</title>
+    </head>
+    <body>
+        <h1>Online Catalog</h1>
+        
+
+    </body>
+</html>
