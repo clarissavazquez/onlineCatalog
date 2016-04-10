482a13c5f66b4125334561a6ea92fe2291dd5d39
negr4882 <snegron@csumb.edu>
2016-04-10 23:11:03 +0000

    Added radio buttons to choose if ascending or descending

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
-
+    session_start();
+    include("../../includes/database.php");
+    $dbConnection = getDatabaseConnection('auto_sale');
+    
+    function sortTable()
+    {
+        
+    }
 ?>
 
 <!DOCTYPE html>
@@ -9,7 +16,12 @@
     </head>
     <body>
         <h1>Online Catalog</h1>
-        
+        <form>
+            In which order:
+            <input type="radio" name="orderBy" value="ASC"> Ascending <br/>
+            <input type="radio" name="orderBy" value="DESC" checked> Descending <br/>
+            <input type="submit" value="Search Products"/>
+        </form>
 
     </body>
 </html>
